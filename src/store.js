85e14061ccfd294e82b5adcd20d02ae6fa2fb98b
Answer1013 <841287025@qq.com>
2019-07-28 14:08:41 +0800

    基本功能完善

--- src/store.js
+++ src/store.js
@@ -5,10 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+      cart:localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[]
   },
   mutations: {
-
+      change(state,payload){
+          state.cart.push(payload);
+          localStorage.setItem("cart",JSON.stringify(state))
+      }
+      
   },
   actions: {
 
